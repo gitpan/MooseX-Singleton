06aaa8fb465a7123a657496738f6ae67f2d64727
Dave Rolsky <autarch@urth.org>
2009-09-12 14:51:15 +0000

    Import of DROLSKY/MooseX-Singleton-0.21 from CPAN.
    gitpan-cpan-distribution: MooseX-Singleton
    gitpan-cpan-version:      0.21
    gitpan-cpan-path:         DROLSKY/MooseX-Singleton-0.21.tar.gz
    gitpan-cpan-author:       DROLSKY
    gitpan-cpan-maturity:     released

--- lib/MooseX/Singleton.pm
+++ lib/MooseX/Singleton.pm
@@ -2,10 +2,11 @@ package MooseX::Singleton;
 
 use Moose 0.82 ();
 use Moose::Exporter;
-use MooseX::Singleton::Object;
-use MooseX::Singleton::Meta::Class;
+use MooseX::Singleton::Role::Object;
+use MooseX::Singleton::Role::Meta::Class;
+use MooseX::Singleton::Role::Meta::Instance;
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 $VERSION = eval $VERSION;
 
 Moose::Exporter->setup_import_methods( also => 'Moose' );
